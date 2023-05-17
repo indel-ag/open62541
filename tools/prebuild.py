b43784aa629aedd4e410c30280d81d11bc4ef242
Daniel Pauli <pauli@indel.ch>
2023-05-17 15:40:44 +0200

    ! Updated prebuild script to current release.

--- tools/prebuild.py
+++ tools/prebuild.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!/opt/indel/bin/indel-python3 -u
 # coding: utf-8
 #-------------------------------------------------------------------------------
 # Script for configuring open62541 library and generating code.
@@ -14,11 +14,12 @@ import subprocess
 #-------------------------------------------------------------------------------
 
 BUILD_DIR = "build"
-ARCHITECTURE = sys.argv[1] if len(sys.argv) > 1 else None
+ARCHITECTURE = sys.argv[1] if len(sys.argv) > 1 else "inos"
 
 OPTIONS = [
-	'UA_ENABLE_PUBSUB',
-	'UA_ENABLE_SUBSCRIPTION_EVENTS',
+	'-DUA_ENABLE_PUBSUB=on',
+	'-DUA_ENABLE_SUBSCRIPTION_EVENTS=on',
+	'-DUA_MULTITHREADING=100',
 ]
 
 #-------------------------------------------------------------------------------
@@ -26,6 +27,10 @@ OPTIONS = [
 #-------------------------------------------------------------------------------
 
 def build_win():
+
+	# TODO this might be deprecated since the last library update since untested since then
+	# if in doubt, use the Linux variant
+
 	CMAKE = "cmake"
 	COMPILER = "Visual Studio 14 2015"
 	MSBUILD = "C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
@@ -40,21 +45,32 @@ def build_win():
 	]
 			
 	# call cmake
-	subprocess.check_call([CMAKE, '-DUA_ARCHITECTURE:STRING=' + ARCHITECTURE, '-G', COMPILER, '..'] + ['-D%s=on' % option for option in OPTIONS], cwd=BUILD_DIR)
+	subprocess.check_call([CMAKE, '-DUA_ARCHITECTURE:STRING=' + ARCHITECTURE, '-G', COMPILER, '..'] + OPTIONS, cwd=BUILD_DIR)
 	# build projects
 	for project in PROJECTS:
 		subprocess.check_call([MSBUILD, project], cwd=BUILD_DIR)
 	# done
 	return 0
-	#return os.system('cd ' + BUILD_DIR + ' && ' + CMAKE + ' -DUA_ARCHITECTURE:STRING=' + ARCHITECTURE + ' -G "' + COMPILER + '" .. && "' + MSBUILD + '" open62541-generator-ids-ns0.vcxproj open62541-generator-namespace.vcxproj')
 # end build_win
 
 #-------------------------------------------------------------------------------
 
 def build_linux():
-	print("TODO implement")
-	#return os.system('cd ' + BUILD_DIR + ' && cmake .. && make')
-	return 1
+	CMAKE = "cmake"
+	
+	# projects required for code generation
+	PROJECTS = [
+		'open62541-code-generation',
+	]
+			
+	# call cmake
+	subprocess.check_call([CMAKE, '-DUA_ARCHITECTURE:STRING=' + ARCHITECTURE, '..'] + OPTIONS, cwd=BUILD_DIR)
+	# build projects
+	for project in PROJECTS:
+		subprocess.check_call(["make", project], cwd=BUILD_DIR)
+
+	# done
+	return 0
 # end build_linux
 
 #-------------------------------------------------------------------------------
